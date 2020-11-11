ee62ac647ee257ca8bcba6599f6efbb392d37b37
Cristian González <crdgonzalezca@unal.edu.co>
2020-11-11 18:33:27 -0500

    Clean up code and measure elapsed time

--- cuda/image_scaling.cu
+++ cuda/image_scaling.cu
@@ -25,33 +25,8 @@ timestamp_t get_timestamp (){
 /**
  * CUDA Kernel Device code
  *
- * Computes the vector addition of A and B into C. The 3 vectors have the same
- * number of elements numElements.
+ * Computes the new scaled output_image with NNS algorithm.
  */
-__global__ void vectorAdd(unsigned char *input_image, unsigned char *output_image, 	int width, int height, 
-    int inputWidthStep, int outputWidthStep){
-    //2D Index of current thread
-	const int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
-    const int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
-
-    if ((xIndex < width) && (yIndex < height)){
-        const int color_tid = yIndex * outputWidthStep + (3 * xIndex);
-
-		//Location of gray pixel in output
-		// const int gray_tid = yIndex * outputWidthStep + xIndex;
-
-		const unsigned char blue = input_image[color_tid];
-		const unsigned char green = input_image[color_tid + 1];
-		const unsigned char red = input_image[color_tid + 2];
-
-		const float gray = red * 0.3f + green * 0.59f + blue * 0.11f;
-
-        output_image[color_tid] = static_cast<unsigned char>(gray);
-        output_image[color_tid + 1] = static_cast<unsigned char>(gray);
-        output_image[color_tid + 2] = static_cast<unsigned char>(gray);
-    }
-}
-
 __global__ void nearest_neighbour_scaling(
     unsigned char *input_image, 
     unsigned char *output_image,
@@ -67,8 +42,6 @@ __global__ void nearest_neighbour_scaling(
     //2D Index of current thread
 	const int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
     const int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
-    //if ((xIndex < 100) && (yIndex < 100))
-    //    printf("%d - %d\n", xIndex, yIndex);
 
     int px = 0, py = 0; 
     int input_width_step = width_input * channels_input;
@@ -76,9 +49,6 @@ __global__ void nearest_neighbour_scaling(
     if ((xIndex < width_output) && (yIndex < height_output)){
         py = ceil(yIndex * y_ratio);
         px = ceil(xIndex * x_ratio);
-            //ptr_source = img.ptr<uchar>(py);
-            
-            // Calculate the value of the i,j pixel for each channel
         for (int channel = 0; channel < channels_output; channel++){
             *(output_image + (yIndex * output_width_step + xIndex * channels_output + channel)) =  *(input_image + (py * input_width_step + px * channels_output +  + channel));
         }
@@ -90,18 +60,17 @@ __global__ void nearest_neighbour_scaling(
  * Host main routine
  */
 int main(int argc, char* argv[]) {
-    // Read parameters 1- source path, 2- Destination path, 3- Number of threads, 4- algorithm
+    // Read parameters 1- source path, 2- Destination path, 3- algorithm
     if (argc != 4) {
         cout << "Arguments are not complete. Usage: image_path image_result_path n_threads algorithm" << endl;
         return 1;
     }
     string source_image_path = argv[1];
     string result_image_path = argv[2];
-    // THREADS = atoi(argv[3]);
     string algorithm = argv[3];
 
     // time measurement variables
-    timestamp_t start, end;
+    timestamp_t start, end, total;
 
     // Error code to check return values for CUDA calls
     cudaError_t err = cudaSuccess;
@@ -114,43 +83,33 @@ int main(int argc, char* argv[]) {
         printf("Error reading image.");
         return 1;
     }
-
+    // Start count time
+    start = get_timestamp();
+    
     // Matrices sizes width * height * 3
     const int input_bytes = input_image.cols * input_image.rows * input_image.channels() * sizeof(unsigned char);
     const int output_bytes = output_image.cols * output_image.rows * output_image.channels() * sizeof(unsigned char);
 
     unsigned char *d_input, *d_output;
     // Allocate the device input image
-//    float *d_A = NULL;
     err = cudaMalloc<unsigned char>(&d_input, input_bytes);
-//    err = cudaMalloc((void **)&d_A, size);
 
     if (err != cudaSuccess) {
         fprintf(stderr, "Failed to allocate device input image (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
 
-    // Allocate the device output image
-//    float *d_B = NULL;
-    err = cudaMalloc<unsigned char>(&d_output, output_bytes);
-
-    if (err != cudaSuccess) {
-        fprintf(stderr, "Failed to allocate device output image (error code %s)!\n", cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-
     // Copy the host input vectors A and B in host memory to the device input vectors in
     // device memory
-    printf("Copy input data from the host memory to the CUDA device\n");
+    //printf("Copy input data from the host memory to the CUDA device\n");
     err = cudaMemcpy(d_input, input_image.ptr(), input_bytes, cudaMemcpyHostToDevice);
-//    err = cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
 
     if (err != cudaSuccess) {
         fprintf(stderr, "Failed to copy input image from host to device (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
 
-    printf("Copy input data from the host memory to the CUDA device\n");
+    //printf("Copy input data from the host memory to the CUDA device\n");
     err = cudaMemcpy(d_output, output_image.ptr(), output_bytes, cudaMemcpyHostToDevice);
 
     if (err != cudaSuccess) {
@@ -158,9 +117,6 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }    
     
-    //int threadsPerBlock = 256;
-    // int blocksPerGrid =(numElements + threadsPerBlock - 1) / threadsPerBlock;
-    // printf("CUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
     int width_input = input_image.cols;
     int height_input = input_image.rows;
     int channels_input = input_image.channels();
@@ -174,7 +130,6 @@ int main(int argc, char* argv[]) {
 	//Calculate numBlocks size to cover the whole image
     const dim3 numBlocks((width_output + threadsPerBlock.x - 1) / threadsPerBlock.x, (height_output + threadsPerBlock.y - 1) / threadsPerBlock.y);
     nearest_neighbour_scaling<<<numBlocks, threadsPerBlock>>>(d_input, d_output, width_input, height_input, channels_input, width_output, height_output, channels_output);
-//    vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, numElements);
     err = cudaGetLastError();
 
     if (err != cudaSuccess) {
@@ -184,9 +139,8 @@ int main(int argc, char* argv[]) {
 
     // Copy the device result vector in device memory to the host result vector
     // in host memory.
-    printf("Copy output data from the CUDA device to the host memory\n");
+    //printf("Copy output data from the CUDA device to the host memory\n");
     err = cudaMemcpy(output_image.ptr(), d_output, output_bytes, cudaMemcpyDeviceToHost);
-    // err = cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
 
     if (err != cudaSuccess) {
         fprintf(stderr, "Failed to copy vector C from device to host (error code %s)!\n", cudaGetErrorString(err));
@@ -208,8 +162,14 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    imwrite(result_image_path, output_image); //write the image to a file
+    // Write the image to a file
+    imwrite(result_image_path, output_image);
 
+    // Finish count time
+    end = get_timestamp();
+    total = end - start + 0.0;
+    float avg_end = avg / MS;
+    printf("Time Elapsed: %f\n", avg_end);
     printf("Done\n");
     return 0;
 }
