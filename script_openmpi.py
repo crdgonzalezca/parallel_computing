4bd8b205687a1a17b6bb2aa313ee7bc53747c75b
Cristian Gonzalez <crdgonzalezca@unal.edu.co>
2020-12-15 01:52:16 +0000

    Run in cluster

--- script_openmpi.py
+++ script_openmpi.py
@@ -21,10 +21,10 @@ with open('results_openmpi.csv', 'w') as f:
                 print(f"Tasks {str(t)}, image: {image}, alg: {algorithm}")
                 time = 0.0
                 for i in range(_ITERATIONS):
-                    command = ["mpirun", "-np", "4", "./image_scaling_openmpi", f"./images/{image}", "./images/image1_480p.jpg", algorithm]
+                    command = ["mpirun","--mca", "plm_rsh_no_tree_spawn",  "1", "-np", "4", "--hostfile", "mpi_hosts", "./image_scaling_openmpi", f"./images/{image}", "./images/image1_480p.jpg", algorithm]
                     completed_process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                     print(completed_process.stdout.decode())
-                    time += float(completed_process.stdout.decode())
+                    time += float(completed_process.stdout.decode().split('\n')[-2])
         
                 avg = time / _ITERATIONS
                 f.write("{:.6f}, {}, {}, {}\n".format(avg, t, image, algorithm))
